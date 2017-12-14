53f52051eb94fe07e7c48ed133b47394eee9599a
AsteriaGamer <asteriagame@gmail.com>
2017-12-14 23:34:11 +0300

    1
    ShitCode

--- computingsystemstatistics.cpp
+++ computingsystemstatistics.cpp
@@ -6,10 +6,25 @@ ComputingSystemStatistics::ComputingSystemStatistics()
 }
 
 void ComputingSystemStatistics::AnalizeSnapShots(){
-    std::map<int,int> programmsStepsDictionary;
+    std::map<int, int> programmsStepsDictionary;
+                for (int i = 0; i < SnapShots.size(); ++i)
+                {
+                    int programmsInSystem = SnapShots[i].ExecutingCount + SnapShots[i].BufferItemsCount;
+                    if (!programmsStepsDictionary.find(programmsInSystem)->first)
+                        programmsStepsDictionary.insert(programmsInSystem, 0);
 
-    for(int i = 0; i < SnapShots.size(); ++i){
-        int programmsInSystem = SnapShots[i].ExecutingCount + SnapShots[i].BufferItemsCount;
-        if(!programmsStepsDictionary(programmsInSystem))
-    }
+                    programmsStepsDictionary[programmsInSystem]++;
+                }
+
+                for (const auto& kv : programmsStepsDictionary) {
+                    ProgrammsCountProbability.insert(kv.first, CountProbability(kv.second, SnapShots.size()));
+
+                }
+
+}
+
+static double ComputingSystemStatistics::CountProbability(int cases, int maxCases)
+{
+        double prob = round(((double)cases / (double)maxCases)*10000)/10000;
+        return prob <= 1 ? prob : 1;
 }
